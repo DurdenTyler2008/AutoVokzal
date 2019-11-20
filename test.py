43789bc280a3646cc3e5b618496ae84f159916d9
Sergio <52253089+DurdenTyler2008@users.noreply.github.com>
2019-11-20 12:22:48 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+from src.AutoVokzal import AutoVokzal,Route,Kassa
+from src.RepairZoone import RepairZone
+
+
+def test_AutoVokzal():
+    autovokzal = AutoVokzal()
+    assert autovokzal
+
+def test_Route():
+    route = Route()
+    assert route
+
+def test_Kassa():
+    kassa = Kassa()
+    assert kassa
+
+def test_RepairZone():
+    repairzone = RepairZone()
+    assert repairzone
